26f6f5b614d464dad07f55ee8c17aa1ea6f08a52
Abe Ahn <abemsq@gmail.com>
2023-07-04 18:53:49 +0900

    change size

--- src/components/section/Home.tsx
+++ src/components/section/Home.tsx
@@ -18,22 +18,22 @@ const Home: React.FC<HomeProps> = ({
       <div className="about" onClick={handleAboutClick}>
         <p>Creating the best digital web experiences. Creating the best digital web experiences. Creating the best digital web experiences.</p>
         <h1>About</h1>
-        <AboutIcon width={30} height={30} color="white"/>
+        <AboutIcon width={24} height={24} color="white"/>
       </div>
       <div className="portfolio" onClick={handlePortfolioClick}>
         <p>View past projects and frameworks used. View past projects and frameworks used. View past projects and frameworks used. </p>
         <h1>Portfolio</h1>
-        <PortfolioIcon width={30} height={30} color="white"/>
+        <PortfolioIcon width={24} height={24} color="white"/>
       </div>
       <div className="blog" onClick={handleBlogClick}>
         <p>View past articles and blogs I wrote. View past articles and blogs I wrote. View past articles and blogs I wrote. View past articles and blogs I wrote.</p>
         <h1>Blog</h1>
-        <BlogIcon width={30} height={30} color="white"/>
+        <BlogIcon width={24} height={24} color="white"/>
       </div>
       <div className="contact" onClick={handleContactClick}>
         <p>For resume or new projects, please contact. For resume or new projects, please contact. For resume or new projects, please contact.</p>
         <h1>Contact</h1>
-        <ContactIcon width={30} height={30} color="white"/>
+        <ContactIcon width={24} height={24} color="white"/>
       </div>
     </div>
   );
