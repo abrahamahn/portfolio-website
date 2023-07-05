b940e55b92a5df8e8e499345615e2fd68fece669
Abe Ahn <abemsq@gmail.com>
2023-07-05 14:29:28 +0900

    change mobile styling

--- src/components/section/Home.tsx
+++ src/components/section/Home.tsx
@@ -15,26 +15,31 @@ const Home: React.FC<HomeProps> = ({
 }) => {
   return (
     <div className="home">
-      <div className="about" onClick={handleAboutClick}>
-        <p>Creating the best digital web experiences. Creating the best digital web experiences. Creating the best digital web experiences.</p>
-        <h1>About</h1>
-        <AboutIcon width={24} height={24} color="white"/>
+      <div className="hero">
+        <p>Creating the best digital web experiences.</p>
       </div>
-      <div className="portfolio" onClick={handlePortfolioClick}>
-        <p>View past projects and frameworks used. View past projects and frameworks used. View past projects and frameworks used. </p>
-        <h1>Portfolio</h1>
-        <PortfolioIcon width={24} height={24} color="white"/>
+      <div className="grid">
+        <div className="about" onClick={handleAboutClick}>
+          <h1>About</h1>
+          <AboutIcon width={24} height={24} color="white"/>
+        </div>
+        <div className="portfolio" onClick={handlePortfolioClick}>
+          <p>View past projects and frameworks used. View past projects and frameworks used. View past projects and frameworks used. </p>
+          <h1>Portfolio</h1>
+          <PortfolioIcon width={24} height={24} color="white"/>
+        </div>
+        <div className="blog" onClick={handleBlogClick}>
+          <p>View past articles and blogs I wrote. View past articles and blogs I wrote. View past articles and blogs I wrote. View past articles and blogs I wrote.</p>
+          <h1>Blog</h1>
+          <BlogIcon width={24} height={24} color="white"/>
+        </div>
+        <div className="contact" onClick={handleContactClick}>
+          <p>For resume or new projects, please contact. For resume or new projects, please contact. For resume or new projects, please contact.</p>
+          <h1>Contact</h1>
+          <ContactIcon width={24} height={24} color="white"/>
+        </div>
       </div>
-      <div className="blog" onClick={handleBlogClick}>
-        <p>View past articles and blogs I wrote. View past articles and blogs I wrote. View past articles and blogs I wrote. View past articles and blogs I wrote.</p>
-        <h1>Blog</h1>
-        <BlogIcon width={24} height={24} color="white"/>
-      </div>
-      <div className="contact" onClick={handleContactClick}>
-        <p>For resume or new projects, please contact. For resume or new projects, please contact. For resume or new projects, please contact.</p>
-        <h1>Contact</h1>
-        <ContactIcon width={24} height={24} color="white"/>
-      </div>
+
     </div>
   );
 };
