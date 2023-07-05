7db2751c1d4e2913a89b5087d557d287d0759e14
Abe Ahn <abemsq@gmail.com>
2023-07-05 21:06:54 +0900

    commit

--- src/components/section/About.tsx
+++ src/components/section/About.tsx
@@ -22,7 +22,7 @@ const About: React.FC = () => {
             <h3>{AboutData.title}</h3>
           </div>
           <div className="info">
-            <p>{AboutData.paragraph1}</p>
+            <p>{AboutData.paragraph1}</p><br/>
             <p>{AboutData.paragraph2}</p>
             <div className="skills">
               {SkillData.map((val, i: number) => (
