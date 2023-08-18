3c921c6e6f470d704c304c071f310ac53611285a
Abe Ahn <abemsq@gmail.com>
2023-08-18 21:30:05 +0900

    fix update

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
-
-import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/store';
-
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/style.scss';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+} else {
+  console.error('Root container not found.');
+}
